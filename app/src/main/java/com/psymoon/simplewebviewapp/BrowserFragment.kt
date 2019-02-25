d6d6ec1d3d36e15f755a756f9d33549f5cfbb649
Simon Chae <chaesmn@gmail.com>
2019-02-25 16:24:07 -0500

    Add WebView to BrowserFragment

--- app/src/main/java/com/psymoon/simplewebviewapp/BrowserFragment.kt
+++ app/src/main/java/com/psymoon/simplewebviewapp/BrowserFragment.kt
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.fragment_browser.*
+
+const val DEFAULT_URL = "https://youtube.com/tv"
 
 class BrowserFragment: Fragment() {
 
@@ -14,5 +18,8 @@ class BrowserFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(DEFAULT_URL)
     }
 }
