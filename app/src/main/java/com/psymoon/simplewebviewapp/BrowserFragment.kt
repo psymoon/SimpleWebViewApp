d6dbd6d77148f8a2204ed5ee25af7c8dcc2f6f45
Simon Chae <chaesmn@gmail.com>
2019-04-04 17:33:14 -0400

    Add android design layout

--- app/src/main/java/com/psymoon/simplewebviewapp/BrowserFragment.kt
+++ app/src/main/java/com/psymoon/simplewebviewapp/BrowserFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.fragment_browser.*
 
@@ -22,6 +23,8 @@ class BrowserFragment: Fragment() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val webView = WebView(context)
 
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
@@ -38,5 +41,7 @@ class BrowserFragment: Fragment() {
         webView.settings.allowFileAccessFromFileURLs = true
 
         webView.loadUrl(DEFAULT_URL)
+
+        browserFrameLayout.addView(webView)
     }
 }
